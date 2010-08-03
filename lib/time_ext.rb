e6bd7fc000e1beddb5cc0688aa461314efb78baa
Jim Myhrberg <contact@jimeh.me>
2010-08-03 14:03:15 +0300

    restructured classes/modules and files

--- lib/time_ext.rb
+++ lib/time_ext.rb
@@ -1,13 +1,8 @@
 require 'rubygems'
 require 'active_support'
-require 'time_ext/method_chain'
-require 'time_ext/core_ext/time/calculations'
-require 'time_ext/core_ext/time/iterations'
 
-module TimeExt
-  
-  module Base
-    TIME_EXT_UNITS = [:year, :month, :day, :hour, :min, :sec, :usec]
-  end
-  
-end
+require 'time_ext/backwards_compatibility'
+require 'time_ext/calculations'
+require 'time_ext/iterations'
+require 'time_ext/method_chain'
+require 'time_ext/core_ext/time'
