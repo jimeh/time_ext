77d9d67ed18978eba3424d70b2ccdd39916c6ddb
Jim Myhrberg <contact@jimeh.me>
2010-08-04 17:28:22 +0300

    merged MethodChain and BackwardsCompatibility modules into a single TimeExt::Support module

--- lib/time_ext.rb
+++ lib/time_ext.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'active_support'
 
-require 'time_ext/backwards_compatibility'
 require 'time_ext/calculations'
 require 'time_ext/iterations'
 require 'time_ext/method_chain'
+require 'time_ext/support'
 require 'time_ext/core_ext/time'
