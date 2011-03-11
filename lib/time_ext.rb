4b1327306d547fbefdac1716cf0d4ba5a3f44ac8
Jim Myhrberg <contact@jimeh.me>
2011-03-11 11:01:39 +0000

    fixed some weird loading/require issues which seemed to be happening on rare occasions when used in a rails project

--- lib/time_ext.rb
+++ lib/time_ext.rb
@@ -1,8 +1,4 @@
 require 'rubygems'
-require 'active_support'
-
-# support both Active Support 2.x and 3.x
-require 'active_support/time' if !Time.respond_to?(:days_in_month)
 
 require 'time_ext/calculations'
 require 'time_ext/iterations'
